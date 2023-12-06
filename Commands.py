bbe48f5f3ab124ae9e299d2475a140315dbe02e1
Kazuma Goto <kazuma.gotoh@jp.ricoh.com>
2023-12-06 14:45:24 +0900

    光源追加

--- Commands.py
+++ Commands.py
@@ -6,12 +6,15 @@ import FreeCAD
 from PySide import QtGui
 import Mesh
 import json
+import Part
 
 _icondir_ = os.path.join(os.path.dirname(__file__), 'resources')
 
 class Subject():
     def __init__(self, fp, base) -> None:
         self.Type = "Subject"
+        fp.Proxy = self
+
         fp.addProperty("App::PropertyLink", "LinkedObject", "Custom", "FreeCAD object to be subject").LinkedObject = base
         # fp.addProperty("App::PropertyString", "UniqueLabel", "Custom", "Label must be unique").UniqueLabel = label
         fp.addProperty("App::PropertyEnumeration", "ElementType", "Custom", "ElementType").ElementType = ["Element", "Compound", "Mixture"]
@@ -21,12 +24,11 @@ class Subject():
         # fp.addProperty("App::PropertyRotation", "Rotation", "Base", "Rotation").Rotation = base.Placement.Rotation
 
         fp.Placement = base.Placement
+        fp.ElementType = "Element"
         fp.setPropertyStatus("Placement", "ReadOnly")
         fp.setPropertyStatus("Label", "ReadOnly")
         # fp.setPropertyStatus("UniqueLabel", "Hidden") # set "-Hidden" to visible
         fp.setPropertyStatus("LinkedObject", "ReadOnly")
-        fp.Proxy = self
-        # self.original_object = obj
         # if hasattr(obj, "CustomProperty") == False:
         #     obj.addProperty("App::PropertyString", "CustomProperty", "MyObject", "A custom property.")
         # pass
@@ -35,7 +37,92 @@ class Subject():
         """
         Called on document recompute
         """        
-        print("Hoge")
+        pass
+
+class LightSource():
+    def __init__(self, fp) -> None:
+        self.Type = "LightSource"
+        fp.Proxy = self
+
+        fp.addProperty("App::PropertyFloat", "Energy", "Custom", "the incident photon energy").Energy = 1.0
+        fp.addProperty("App::PropertyEnumeration", "EnergyUnit", "Custom", "the unit of energy corresponding to anEnergy.").EnergyUnit = ["eV", "keV", "MeV"]
+        fp.addProperty("App::PropertyInteger", "aNumberOfPhotons", "Custom", "the number of incident photons.").aNumberOfPhotons = 1000
+
+        fp.EnergyUnit = "keV"
+        fp.setPropertyStatus("Label", "ReadOnly")
+
+    def execute(self, obj):
+        """
+        Called on document recompute
+        """        
+        obj.Shape = Part.makeSphere(1)
+
+class ViewProviderLightSource:
+
+    def __init__(self, obj):
+        """
+        Set this object to the proxy object of the actual view provider
+        """
+
+        obj.Proxy = self
+
+    def attach(self, obj):
+        """
+        Setup the scene sub-graph of the view provider, this method is mandatory
+        """
+        return
+
+    def updateData(self, fp, prop):
+        """
+        If a property of the handled feature has changed we have the chance to handle this here
+        """
+        return
+
+    def getDisplayModes(self,obj):
+        """
+        Return a list of display modes.
+        """
+        return []
+
+    def getDefaultDisplayMode(self):
+        """
+        Return the name of the default display mode. It must be defined in getDisplayModes.
+        """
+        return "Shaded"
+
+    def setDisplayMode(self,mode):
+        """
+        Map the display mode defined in attach with those defined in getDisplayModes.
+        Since they have the same names nothing needs to be done.
+        This method is optional.
+        """
+        return mode
+
+    def onChanged(self, vp, prop):
+        """
+        Print the name of the property that has changed
+        """
+
+        FreeCAD.Console.PrintMessage("Change property: " + str(prop) + "\n")
+
+    def getIcon(self):
+        """
+        Return the icon in XMP format which will appear in the tree view. This method is optional and if not defined a default icon is shown.
+        """
+
+        return
+
+    def __getstate__(self):
+        """
+        Called during document saving.
+        """
+        return None
+
+    def __setstate__(self,state):
+        """
+        Called during document restore.
+        """
+        return None
 
 class CreateSubjectCommand():
     '''This class will be loaded when the workbench is activated in FreeCAD. You must restart FreeCAD to apply changes in this class'''  
@@ -102,6 +189,40 @@ class CreateSubjectCommand():
         elif hasattr(obj, 'Group') and obj.Group:
             for child in obj.Group:
                 self.process_object(child)
+
+class CreateLightSourceCommand():
+
+    def __init__(self) -> None:
+        self.lightsource_name = f"LightSource"
+
+    def Activated(self):
+        '''Will be called when the feature is executed.'''
+
+        # カスタムPartを生成
+        fp = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", self.lightsource_name)
+        LightSource(fp)
+        ViewProviderLightSource(fp.ViewObject)
+
+        FreeCAD.ActiveDocument.recompute()
+
+    def IsActive(self):
+        '''Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional.'''
+        if FreeCAD.ActiveDocument is None:
+            return False
+        
+        if FreeCAD.ActiveDocument.getObject(self.lightsource_name):
+            # FreeCAD.Console.PrintMessage(f"already created.\n")
+            return False
+        
+        return True
+        
+    def GetResources(self):
+        '''Return the icon which will appear in the tree view. This method is optional and if not defined a default icon is shown.'''
+        return {'Pixmap'  : os.path.join(_icondir_, 'template_resource.svg'),
+                'Accel' : '', # a default shortcut (optional)
+                'MenuText': 'LightSource',
+                'ToolTip' : 'Create a light source.' }               
 
 class ExportAsStlFilesCommand():
     '''This class will be loaded when the workbench is activated in FreeCAD. You must restart FreeCAD to apply changes in this class'''  
@@ -212,6 +333,7 @@ class ExportAsStlFilesCommand():
 
 Gui.addCommand('Export(stl files)', ExportAsStlFilesCommand())
 Gui.addCommand('CreateSubject', CreateSubjectCommand())
+Gui.addCommand('CreateLightSource', CreateLightSourceCommand())
 
 # デバッグ用 不要になったらコメントアウトする
 # import ptvsd
